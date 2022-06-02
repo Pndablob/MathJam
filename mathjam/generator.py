72bfe6abd50bfcdd32347ad90476f7db05eaace0
Pnda <64608661+Pndablob@users.noreply.github.com>
2022-06-01 23:18:03 -0400

    feature testing 6/1/22 evening

--- mathjam/generator.py
+++ mathjam/generator.py
@@ -1,8 +1,19 @@
 from mathjam.utils import *
+from sympy import *
 
 master = Tk()
-deg = randint(0, 10)
-poly = genPoly(deg)
-print(poly)
+
+deg = randint(0, 9)
+func = genFunc(deg)
+
+poly = func.replace("**", "^").replace("*", "")
+print(f"Function: {poly}")
 displayLatex(master, poly)
+
+x = Symbol("x")
+ans = str(integrate(func, x))
+ans = ans.replace("**", "^").replace("*", "")
+print(f"Answer: {ans} + C")
+displayLatex(master, f"{ans} + C")
+
 master.mainloop()
--- mathjam/utils.py
+++ mathjam/utils.py
@@ -17,7 +17,7 @@ def displayLatex(master, equation):
     ax = fig.add_subplot(111)
 
     ax.clear()
-    ax.text(0.2, 0.6, equation, fontsize=16)
+    ax.text(0.05, 0.2, equation, fontsize=16)
 
     canvas = FigureCanvasTkAgg(fig, master=label)
     canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
@@ -29,24 +29,33 @@ def displayLatex(master, equation):
     canvas.draw()
 
 
-def genPoly(deg):
-    p = ""
+# generate a random polynomial
+def genFunc(deg):
+    f = ""
     for i in range(deg, 0, -1):
-        coef = randint(0, 10)
+        coef = randint(1, 10)  # editable values
         if coef > 1:
-            p += f"{coef}"
-        p += "x"
+            f += f"{coef}*"
+        f += "x"
         if i > 1:
-            p += f"^{i}"
-        sign = randint(0, 1)
-        if sign == 1 and p != "" and coef != 0:
-            p += "+"
+            f += f"^{i}"
+        sign = randint(0, 1)  # editable values
+        if sign == 1:
+            f += " + "
         elif sign == 0:
-            p += "-"
-    c = randint(1, 50)
-    p += str(c)
+            f += " - "
+    c = randint(1, 50)  # editable values
+    f += str(c)
 
-    return p
+    return f
+
+
+def latexToMath(latex):
+    return latex.replace("x", "*x").replace("^", "**")
+
+
+def mathToLatex(math):
+    return math.replace("**", "^").replace("*", "")
 
 
 def main():
